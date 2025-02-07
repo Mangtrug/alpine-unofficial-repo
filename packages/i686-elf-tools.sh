2c5f22e100de2bfd2e74a239ba5aae5324cc77fd
Mangtrug <100173735+Mangtrug@users.noreply.github.com>
2025-02-07 18:54:17 -0300

    Update i686-elf-tools.sh

--- packages/i686-elf-tools.sh
+++ packages/i686-elf-tools.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# Função para verificar dependências
 check_dependency() {
     local package="$1"
     
@@ -12,8 +14,27 @@ check_dependency() {
     fi
 }
 
+# Garantir que o diretório de cache exista
+mkdir -p "$CACHE_DIR"
+
+# Mudar para o diretório de cache
+cd "$CACHE_DIR" || { echo -e "\e[1;31mErro ao acessar o diretório de cache.\e[0m"; exit 1; }
+
+# Verificar dependências
 check_dependency wget
 check_dependency unzip
 check_dependency zip
 
-wget https://github.com/lordmilko/i686-elf-tools/releases/download/13.2.0/i686-elf-tools-linux.zip
+# Baixar o arquivo
+echo -e "\e[1;32mBaixando i686-elf-tools...\e[0m"
+wget https://github.com/lordmilko/i686-elf-tools/releases/download/13.2.0/i686-elf-tools-linux.zip || { echo -e "\e[1;31mErro ao baixar o arquivo.\e[0m"; exit 1; }
+
+# Extrair o arquivo
+echo -e "\e[1;32mExtraindo o arquivo...\e[0m"
+unzip i686-elf-tools-linux.zip || { echo -e "\e[1;31mErro ao extrair o arquivo.\e[0m"; exit 1; }
+
+# Limpar o arquivo zip
+echo -e "\e[1;32mLimpando o arquivo zip...\e[0m"
+rm -f i686-elf-tools-linux.zip
+
+echo -e "\e[1;32mInstalação concluída com sucesso.\e[0m"
