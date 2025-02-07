87254746e9a067aee7e3e8ce1fb27751a009a477
Mangtrug <100173735+Mangtrug@users.noreply.github.com>
2025-02-07 17:21:07 -0300

    Update i686-elf-tools.sh

--- packages/i686-elf-tools.sh
+++ packages/i686-elf-tools.sh
@@ -1,1 +1,19 @@
+#!/bin/sh
+check_dependency() {
+    local package="$1"
+    
+    # Verifica se o pacote está instalado
+    if apk info "$package" &> /dev/null; then
+        echo -e "\e[1;34m>> O pacote $package está instalado.\e[0m"
+    else
+        echo -e "\e[1;31m> O pacote $package não está instalado.\e[0m"
+        echo "Tentando instalar $package..."
+        apk add "$package" || { echo -e "\e[1;31mErro ao instalar $package. Abortando.\e[0m"; exit 1; }
+    fi
+}
+
+check_dependency wget
+check_dependency unzip
+check_dependency zip
+
 wget https://github.com/lordmilko/i686-elf-tools/releases/download/13.2.0/i686-elf-tools-linux.zip
