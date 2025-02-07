7ef476166f5c1e0b9a25212fe4a7f8d32b866262
Mangtrug <100173735+Mangtrug@users.noreply.github.com>
2025-02-06 21:51:20 -0300

    Update neofetch.sh

--- packages/neofetch.sh
+++ packages/neofetch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Função para verificar se o pacote está instalado
 check_dependency() {
     local package="$1"
@@ -7,20 +9,26 @@ check_dependency() {
         echo -e "\e[1;34m>> O pacote $package está instalado.\e[0m"
     else
         echo -e "\e[1;31m> O pacote $package não está instalado.\e[0m"
-        echo "tentando instalar $package..."
-        apk add "$package"
+        echo "Tentando instalar $package..."
+        apk add "$package" || { echo -e "\e[1;31mErro ao instalar $package. Abortando.\e[0m"; exit 1; }
     fi
 }
 
-echo -e "\e[1;32m>> verificando dependencias...\e[0m"
+echo -e "\e[1;32m>> Verificando dependências...\e[0m"
 check_dependency unzip
 check_dependency zip
 
-echo -e "\e[1;32m>> baixando...\e[0m"
-wget https://github.com/dylanaraps/neofetch/archive/refs/tags/7.1.0.zip
+echo -e "\e[1;32m>> Baixando Neofetch...\e[0m"
+wget -q https://github.com/dylanaraps/neofetch/archive/refs/tags/7.1.0.zip || { echo -e "\e[1;31mErro ao baixar o arquivo.\e[0m"; exit 1; }
 
-echo -e "\e[1;32m>> descompactando 7.1.0.zip...\e[0m"
-unzip 7.1.0.zip
-rm -rf 7.1.0.zip*
+echo -e "\e[1;32m>> Descompactando 7.1.0.zip...\e[0m"
+unzip 7.1.0.zip || { echo -e "\e[1;31mErro ao descompactar o arquivo.\e[0m"; exit 1; }
+rm -f 7.1.0.zip  # Remove o arquivo zip após extrair
 
-echo -e "\e[1;32m>> copiando binario para /usr/local/bin\e[0m"
+echo -e "\e[1;32m>> Copiando binário para /usr/local/bin...\e[0m"
+cp neofetch-7.1.0/neofetch /usr/local/bin || { echo -e "\e[1;31mErro ao copiar o binário.\e[0m"; exit 1; }
+
+echo -e "\e[1;32m>> Limpando arquivos temporários...\e[0m"
+rm -rf neofetch-7.1.0
+
+echo -e "\e[1;32m>> Script concluído com sucesso!\e[0m"
